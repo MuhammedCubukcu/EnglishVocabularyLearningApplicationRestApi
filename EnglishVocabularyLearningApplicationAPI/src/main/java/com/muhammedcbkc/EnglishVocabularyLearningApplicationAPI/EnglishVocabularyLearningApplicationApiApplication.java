dc6a54f66f897ced988d233757648e26c16b184b
MuhammedCubukcu <muhammedcubukcu0@gmail.com>
2025-01-09 22:41:58 +0300

    Initialize English Vocabulary Learning Application API with basic structure and configuration

---
+++ EnglishVocabularyLearningApplicationAPI/src/main/java/com/muhammedcbkc/EnglishVocabularyLearningApplicationAPI/EnglishVocabularyLearningApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.muhammedcbkc.EnglishVocabularyLearningApplicationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishVocabularyLearningApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishVocabularyLearningApplicationApiApplication.class, args);
+	}
+
+}
---
+++ EnglishVocabularyLearningApplicationAPI/src/test/java/com/muhammedcbkc/EnglishVocabularyLearningApplicationAPI/EnglishVocabularyLearningApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhammedcbkc.EnglishVocabularyLearningApplicationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishVocabularyLearningApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
